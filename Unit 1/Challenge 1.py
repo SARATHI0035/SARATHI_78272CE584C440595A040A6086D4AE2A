43cd73e4f85e8d6afa16b6af4182d0910c7b3c6a
SARATHI0035 <140004512+SARATHI0035@users.noreply.github.com>
2023-09-06 10:58:30 +0530

    Update Challenge 1.py

--- Unit 1/Challenge 1.py
+++ Unit 1/Challenge 1.py
@@ -1,12 +1,10 @@
-def isleapyear(year):
-  if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
-    return True
+def fact_rec(n):
+  if n == 0 or n == 1:
+    return 1
   else:
-    return False
+    return n * fact_rec(n - 1)
 
 
-year = int(input("Enter the year :"))
-if isleapyear(year):
-  print("{} is a leap year.".format(year))
-else:
-  print("{} is not a leap year.".format(year))
+number = int(input("Enter the number :"))
+res = fact_rec(number)
+print("The factorial of {} is {}.".format(number, res))
